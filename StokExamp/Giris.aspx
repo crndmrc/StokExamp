54bac8077ea4f6e81946439e3f92ad723982e51c
crndmrc <ceren_57_99@hotmail.com>
2022-03-15 17:34:56 +0300

    Initial commit

---
+++ StokExamp/Giris.aspx
@@ -0,0 +1,72 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Giris.aspx.cs" Inherits="StokExamp.Giris" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            height: 30px;
+        }
+    </style>
+</head>
+<body>
+   <form id="form1" runat="server">
+        <table>
+            <tr>
+                <td class="auto-style1">
+                    <asp:Button ID="btnListele" runat="server" Text="Listele" Width="75px" OnClick="btnListele_Click" />
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:GridView ID="gvMusteri" runat="server" DataKeyNames="id"  OnSelectedIndexChanged="gvMusteri_SelectedIndexChanged">
+                        <Columns>
+                            <asp:CommandField ShowSelectButton="True" />
+                        </Columns>
+                    </asp:GridView>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <table>
+                        <tr>
+                            <th colspan="3">
+                                <b>Kayıt Formu</b>
+                            </th>
+                        </tr>
+                        <tr>
+                            <td>Id</td>
+                            <td>
+                                <asp:Label ID="lblId" runat="server"></asp:Label>
+                            </td>
+                            <td>
+                                <asp:Button ID="btnYeni" runat="server" Text="Yeni" Width="75px" OnClick="btnYeni_Click" />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Ad</td>
+                            <td>
+                                <asp:TextBox ID="txtAd" runat="server"></asp:TextBox>
+                            </td>
+                            <td>
+                                <asp:Button ID="btnKaydet" runat="server" Text="Kaydet" Width="75px" OnClick="btnKaydet_Click" style="height: 26px" />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Telefon</td>
+                            <td>
+                                <asp:TextBox ID="txtTelefon" runat="server"></asp:TextBox>
+                            </td>
+                            <td>
+                                <asp:Button ID="btnSil" runat="server" Text="Sil" Width="75px" OnClick="btnSil_Click" />
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
---
+++ StokExamp/Giris.aspx.cs
@@ -0,0 +1,119 @@
+﻿using StokExamp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace StokExamp
+{
+    public partial class Giris : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            buttonEnableAyarla();
+            if (IsPostBack) return;
+            formuTemizle();
+        }
+        private void formuTemizle()
+        {
+            lblId.Text = "";
+            txtAd.Text = "";
+            txtTelefon.Text = "";
+        }
+        private void buttonEnableAyarla()
+        {
+            if (gvMusteri.SelectedIndex >= 0)
+            {
+                btnSil.Enabled = true;
+            }
+            else
+            {
+                btnSil.Enabled = false;
+            }
+        }
+        protected void btnListele_Click(object sender, EventArgs e)
+        {
+            STOKEntities1 _context = new STOKEntities1();
+            gvMusteri.DataSource = _context.tbl_Musteri.ToList();
+            gvMusteri.DataBind();
+
+            gvMusteri.SelectedIndex = -1;
+            gvMusteri_SelectedIndexChanged(null, null);
+        }
+
+        protected void gvMusteri_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (gvMusteri.SelectedIndex >= 0)
+            {
+                int seciliId = int.Parse(gvMusteri.SelectedValue.ToString());
+
+                STOKEntities1 _context = new STOKEntities1();
+                tbl_Musteri _musteri = _context.tbl_Musteri.Find(seciliId);
+
+                lblId.Text = _musteri.id.ToString();
+                txtAd.Text = _musteri.adi;
+                txtTelefon.Text = _musteri.telefon.ToString();
+            }
+            else
+            {
+                formuTemizle();
+            }
+            buttonEnableAyarla();
+        }
+
+        protected void btnYeni_Click(object sender, EventArgs e)
+        {
+            gvMusteri.SelectedIndex = -1;
+            formuTemizle();
+            buttonEnableAyarla();
+            txtAd.Focus();
+        }
+
+        protected void btnKaydet_Click(object sender, EventArgs e)
+        {
+            if (lblId.Text == "") 
+            {
+                STOKEntities1 _context = new STOKEntities1();
+                tbl_Musteri _musteri = new tbl_Musteri();
+                _musteri.adi = txtAd.Text;
+                _musteri.telefon = int.Parse(txtTelefon.Text);
+
+                _context.tbl_Musteri.Add(_musteri);
+                _context.SaveChanges();
+
+                btnListele_Click(null, null);
+            }
+            else
+            {
+                STOKEntities1 _context = new STOKEntities1();
+                tbl_Musteri _musteri = _context.tbl_Musteri.Find(int.Parse(lblId.Text));
+                _musteri.adi = txtAd.Text;
+                _musteri.telefon = int.Parse(txtTelefon.Text);
+                _context.SaveChanges();
+
+                btnListele_Click(null, null);
+            }
+
+            buttonEnableAyarla();
+        }
+
+        protected void btnSil_Click(object sender, EventArgs e)
+        {
+            if (lblId.Text != "")
+            {
+                STOKEntities1 _context = new STOKEntities1();
+                tbl_Musteri _musteri = _context.tbl_Musteri.Find(int.Parse(lblId.Text));
+
+                _context.tbl_Musteri.Remove(_musteri);
+                _context.SaveChanges();
+                btnListele_Click(null, null);
+            }
+
+            buttonEnableAyarla();
+        }
+
+
+    }
+}
